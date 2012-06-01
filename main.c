a3090b51cb5808512e42d1d506c5ef4a9d4b6295
Silica <1484702+Silica@users.noreply.github.com>
2012-06-02 04:13:15 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,59 @@
-#include <stdio.h>
-
-int main(void)
-{
-	char* s;
-	s = "128" + ('3' - '1');
-	printf(("%d%s\n", 8, s));
-	return 0;
-}
-
+                                           Z;_=&Z;XXXXXXXXXXX;X;P();A;B;C                                                   
+                                           ;D;E;F;G;a;b;c;d;e;f;g;h;i;j                                                     
+                                          ;k;l;m;n;o;p;q;r;s;t;u;v;w;                                                       
+                                           x;y;z;XXXX;  XXXXXXXXXXX;                                                        
+                                            #include      <stdio.h>                                                         
+                                            ;(*Q)()=       printf;;                                                         
+                                             int main(void){++F;a=F++;b=F++;c=F++;d=F++;e=F++;                              
+                                          f=F++;g=F++;h=F++;i=F++;A=F++;B=F++;C=F++;D=F++;E=F++;j=d                         
+                                    *d;l=j*j;m=l*j;n=l*l;k=n*j;o=c*l;p=E*m;q=h*k;r=E*j;s=h*m;t=h*j;u=a*                     
+                                n;v=A*k;w=c*n;x=E*k;y=A*j;z=b*j;G=q|u|p|o;Z=v|h*n|p|h*l|d*j|c;P();Z=v|A*n|p                 
+                             |h*l|t;P();Z=G|i*j|E;P();Z=i*k|e*n|p|e*l|y|E;P();Z=v|u|p|i*l|t|F;P();Z=x|w|b*m|h*              
+                          l|t|C;P();Z=x|w|i*m|D*l|t|a;P();Z=c*k|u|A*m|E*l|t|a;P();Z=G|z|i;P();Z=G|y|g;P();Z=q|p|o           
+                        |i*j|i;P();Z=f*k|D*n|A*l|t|b;P();Z=f*k|E*n|f*m|i*l|f*j|a;P();Z=x|w|f*m|o|z|E;P();Z=x|h*n|i*         
+                      m|b*l|t|b;P();Z=x|w|A*m|D*l|y|A;P();Z=x|w|i*m|o|t|b;P();Z=x|w|A*m|g*l|t|a;P();Z=x|e*n|s|a*l|t;P       
+                    ();Z=x|e*n|B*m|A*l|t|g;P();Z=x|g*n|i*m|B*l|t|A;P();Z=x|f*n|i*m|B*j|e;P();Z=x|w|i*m|C*l|i*j|E;P();Z      
+                   =x|g*n|i*m|b*l|t|b;P();Z=x|w|i*m|d*l|y|D;P();Z=x|w|s|h*l|t|a;P();Z=x|w|A*m|f*l|t|a;P();Z=x|w|s|F*l|t|    
+                 a;P();Z=x|w|A*m|t|a;P();Z=x|w|i*m|e*l|t|a;P();Z=x|w|s|d*l|t|a;P();Z=A*n|s|b*l|t;P();Z=v|w|A*m|D*l|r|f;P(   
+                );Z=v|w|A*m|g*l|r|h;P();Z=v|F*n|s|a*l|r|c;P();Z=g*k|w|f*m|E*l|f*j|a;P();Z=g*k|b*n|f*m|e*l|g*j|g;P();Z=i*k|  
+               e*n|s|o|r|c;P();Z=v|u|s|b*l|r|c;P();Z=v|d*n|s|b*l|r|c;P();Z=v|B*n|s|o|r|c;P();Z=v|B*n|s|a*l|r|c;P();Z=B*k|h*n
+              |i*m|B*l|r|f;P();Z=q|B*n|s|a*l|r|c;P();Z=q|C*n|s|b*l|r|c;P();Z=v|f*n|s|a*l|r|c;P();Z=q|d*n|s|a*l|r|c;P();Z=B*k
+             |E*n|s|a*l|r|c;P();Z=i*k|i*n|s|a*l|r|c;P();Z=q|b*n|s|r|c;P();Z=q|g*n|p|h*l|A;P();Z=q|A*n|p|e*l|y|A;P();Z=G|i*j|
+             B;P();Z=i*k|B*n|p|e*l|y|g;P();Z=v|D*n| p|g*l|i*j|E;P();Z=G|   i*j|d;P       ();Z=G|i*j|g;P();Z=G|i*j|F;P();Z=G|
+            y|E;P();Z=G|y|a;P();Z=q|p|o|y|B;P();Z=v  |D*n|p|f*            l|t|               a;P();Z=v|g*n|p|h*l|y|c;P();Z=G
+            |y|c;P();Z=G|t|B;P();Z=G|y|i;P();Z=G|t|f  ;P               ()                      ;Z=q|F*n|p|e*l|t|B;P();Z=q|e*
+           n|p|g*l|t|b;P();Z=G|y|g;P();Z=G|i*j|   g;P               ();                         Z=G|y|f;P();Z=G|t|F;P();Z=G|
+           y;P();Z=G|i*j|e;P();Z=q|p|o|t|                         d;                            P();Z=A*n|A*l|t|b;P();Z=i*k|
+           e*n|s|o|r|c;P();Z=v|u|s|b*l                                                          |r|c;P();Z=v|d*n|s|b*l|r|c;P
+           ();Z=v|B*n|s|o|r|c;P();                                           Z=q|B*n|A*m|g       *l|r|f;P();Z=i*k|s|h*l|r|f;
+           P();Z=i*k|A*n|B*m|C*l|r|F;P(                                   );Z=           b*k|    b*m|A;P();Z=e*k|b*n|b*m|z;P
+           ();Z=d*k|d*n|d*m|a                                            *l    |d*j|e;P()   ;Z=   b*k|d*m|e*l|d*j|D;P();;;Z=
+            b*k|b*m|z;P();Z                                            =b     *k|b*m|z;P();   Z= b*k|b*m|z;P();Z=v|C*n|i*m|C
+            *l|r|f;P();Z=i                                             *     k|e*n|s|o|r|c;P   ();Z=v|u|s|b*l|r|c;P();Z=v|d*
+            n|s|b*l|r|c;P          ();Z=v|B*n|                         s    |o|r  |c;P();Z=b*   k|b*m|A;P();Z=f*k|D*n|b*m|z;
+             P( );Z=f*k          |E*n        |f*                            m|i*l|f*j|a;P();Z   =b*k|f*m|o|z|E;P();Z=b*k|b*m
+             |z ;P();Z=         b*k|          b*m                           |z;P();Z=b*k|b*m|   z;P();Z=q|F*n|i*m|e*l|r|e;P(
+             );  Z=q|C*        n|      A*m|a*   l                           |r|i;P();Z=i*k|g*    n|s|o|r|c;P();Z=v|D*n|s|o|r
+              |  c;P();       Z=     B*k|s|b*l|r                             |c;P();Z=v|i*n|s   |o|r|c;P();Z=v|s|o|r|c;P();Z
+                  =b*k|      b       *m|A;P();Z=f                            *k|u|b*m|z;P();Z   =g*k|g*n|g*m|o|f*j|E;P();Z=b
+                   *k|g*    m|      b* l|f*j|e;P()                            ;Z=b*k|b*m|z;P(   );Z=b*k|b*m|z;P();Z=b*k|b*m|
+                    z;P(    )       ;Z=v|w|A*m|D*l                             |r|f;P();Z=v|    w|A*m|g*l|r|h;P();Z=v|E*n|s|
+                    a*l|r   |       c;P();Z=B*k|A*                               n|s|g*l|r|     e;P();Z=i*k|B*n|s|A*l|r|e;P(
+                   );Z=i*k  |B      *m|e*l|r|g;P()                                              ;Z=i*k|C*n|i*m|E*l|r|f;P();Z
+                   =q|g*n|   p       |h*l|z|h;P();                                              Z=q|A*n|p|e*l|y|A;P();Z=G|i*
+                   j|B;P();  Z=      i*k|B*n|p|e*l                                             |y|g;P();Z=v|D*n|p|g*l|i*j|E;
+                   P();Z=G|            i*j|d;P();                                              Z=G|i*j|g;P();Z=G|i*j|F;P();Z
+                  =G|y|E;P(             );Z=q|                                                       F*n|p|e*l|y|a;P();Z=q|e
+                  *n| p|g*l                                                                           |t|b;P();Z=G|y|g;P();Z
+                 =G|  i*j|g                                                                         ;P();Z=G|y|f;P();Z=G|t|F
+                 ;P(  );Z=G                                                                        |y;P();Z=G|i*j|e;P();Z=A*
+                n|    b*m|i                                                                      *l|t|d;P();Z=f*k|F*n|d*m|o|
+               A;    P();Z=g                             *k|b*n|g*                             m|i*l|g*j;P();Z=f*k|h*n|f*m|g
+             *l|    f*j|i;P();                                                              Z=b*k|h*n|b*m|g*j|d;P();Z=x|w|b*
+             m        |i*l|d*j|c                                                         ;P();Z=x|w|s|F*l|t|a;P();Z=x|w|s|d*
+                           l|t|a;P(                                                  );Z=x|w|A*m|A*l|t|a;P();Z=x|w|A*m|a*l|t
+                           |a;P    ();                                          Z=x|w|s|o|t|b;P();Z=b*k|i*m|o|t|b;P();Z=g*k|
+                           g*n|e     *m|d*l|z                               |h;P(   );Z=g*k|d*n|g*m|d*l|f*j|i;P();Z=c*k|A*n|
+                           g*m|     b*      l|f*j|e;P();                Z=f*k|B     *n|d*m|z;P();Z=f*k|E*n|f*m|i*l|g*j|e;P()
+                           ;Z=g                         *k|d*n|e*m|F*l|f*             j|a;P();Z=g*k|w|g*m|o|f*j|e;P();Z=A*n|
+                           b*m                                                           |i*l|f*j|F;P();return X;}P(){Q(_);}
