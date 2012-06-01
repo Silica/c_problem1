236c2fcbdde89bc0c7c5369bfe4e00bda7874c17
Kuina <kuina@tes.so>
2012-06-01 21:23:16 +0900

    meooow!

--- main.c
+++ main.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
 
-int f0(int n)
-{
-	return n;
-}
-
-int f1(int n)
-{
-	return n * n;
-}
-
-void swap(int(**a)(), int(**b)())
-{
-	int(*temp)() = *a;
-	*a = *b;
-	*b = temp;
-}
-
 int main(void)
 {
-	int(*p)() = f0;
-	int(*q)() = f1;
-	void* pp = p;
-	void* qq = q;
-	printf("%d\n", p(5));
-	printf("%d\n", q(5));
-	swap(&pp, &qq);
-	printf("%d\n", p(5));
-	printf("%d\n", q(5));
+	char* s;
+	s = "128" + '3' - '1';
+	printf(("%d%s\n", 8, s));
 	return 0;
 }
 
